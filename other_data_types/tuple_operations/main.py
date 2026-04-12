4715a86053836cc1853f7665a21ed33d6b71e77d
imkerberoz <>
2026-04-12 04:37:17 +0000

    Tuple Operations

--- other_data_types/tuple_operations/main.py
+++ other_data_types/tuple_operations/main.py
@@ -7,13 +7,13 @@ shelf1_update = ["tomatoes", "celery", "cilantro"]
 shelf1_update_tuple = tuple(shelf1_update)
 #Concatenate
 shelf1_concat = shelf1 + shelf1_update_tuple 
-print("Updated Shelf:", shelf1_concat)
+print("Updated Shelf #1:", shelf1_concat)
 #Count "celery"
 celery_count = shelf1_concat.count("celery")
 celery_index = shelf1_concat.index("celery")
 
 print("Number of Celery:", celery_count)
-print("Number of Index::", celery_index)
+print("Celery Index:", celery_index)
 
 
 
